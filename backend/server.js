b4bc1d44831ad96247f63f4a849c61356f7c49aa
rajeev <rajeevchoudhary2425@gmail.com>
2026-01-10 15:41:15 +0530

    changed the port

--- backend/server.js
+++ backend/server.js
@@ -86,6 +86,6 @@ io.on("connection",(socket)=>{
         delete users[socket.id];
     })
 })
-server.listen(3000,()=>{
-    console.log("server listening on port 3000");
+server.listen(5000,()=>{
+    console.log("server listening on port 5000");
 })
